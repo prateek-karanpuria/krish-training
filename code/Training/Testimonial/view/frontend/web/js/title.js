41794c4645d8b21a2b9cb89156cf91b8e96134a5
prateek.karanpuria <prateek.karanpuria@krishtechnolabs.com>
2019-10-04 17:21:35 +0530

    4 Oct 2019 progress

--- code/Training/Testimonial/view/frontend/web/js/title.js
+++ code/Training/Testimonial/view/frontend/web/js/title.js
@@ -2,6 +2,8 @@ define(function() {
    'use strict';
 
    return function(config, element) {
+     console.log("Config: ",config);
+     console.log("Element: ",element);
      element.innerText = 'Version 1';
    } 
 });
