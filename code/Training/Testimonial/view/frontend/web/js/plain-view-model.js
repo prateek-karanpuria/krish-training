9429885a97cb54134565f13fbdfd75d9506b09b6
prateek.karanpuria <prateek.karanpuria@krishtechnolabs.com>
2019-10-07 19:14:27 +0530

    7 Oct progress

--- code/Training/Testimonial/view/frontend/web/js/plain-view-model.js
+++ code/Training/Testimonial/view/frontend/web/js/plain-view-model.js
@@ -1,14 +1,27 @@
-define([], function() {
+define(['ko'], function(ko) {
     "use strict";
 
-    return function(config, data) {
+    return function(config) {
+        var data = ko.observable("Data");
+            data.subscribe(function(newValue) {
+                console.log("Data value changed to " + newValue);
+            });
+
+            data.subscribe(function(oldValue) {
+                console.log("Data value changed from " + oldValue);
+            }, this, 'beforeChange');
+
         return {
-            'title': 'Test',
+            'title': ko.observable('Test'),
             getTitle: function() {
                 return this.title;
             },
             config: config,
-            'data': data
+            data: data,
+            output: ko.computed(function() {
+                console.log('In Computed observable function');
+                return 'abc';
+            })
         };
-    };
+    }; 
 });
