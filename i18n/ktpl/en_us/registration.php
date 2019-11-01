b8ee20b4766414e57a88ba93eec1b0a29ea5e4c4
prateek.karanpuria <prateek.karanpuria@krishtechnolabs.com>
2019-11-01 19:29:13 +0530

    translation module

---
+++ i18n/ktpl/en_us/registration.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Language translation module registration file
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::LANGUAGE,
+    'ktpl_en_us',
+    __DIR__
+);
